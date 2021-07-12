1fbda19757777fd5783328183decdbf45873574d
MaryVPie <pirogova.mariia@gmail.com>
2021-07-12 15:14:15 -0700

    added function and styles to fetches UV data

--- script.js
+++ script.js
@@ -14,11 +14,37 @@ function fetchCurrentWeather(city) {
         currentWeatherContainer.innerHTML = `<h2>${y.name}</h2> <img id="wicon" src="http://openweathermap.org/img/w/${weatherConditions.icon}.png" alt="Weather icon">
         <br>Temp:&nbsp;${kelvinToFahrenheit(y.main.temp)}&deg;F<br>
         <br>Wind:&nbsp;${y.wind.speed} MPH<br>
-        <br>Humidity:&nbsp;${y.main.humidity} %<br>
-        <br>UV Index:&nbsp;<span class="badge bg-success">Success</span><br><br>`;
+        <br>Humidity:&nbsp;${y.main.humidity} %<br>`;
+        
+        // <br>UV Index:&nbsp;<span class="badge bg-success">${}</span><br><br>`;
+
+        //calls UV data function
+        fetchesUV(y.coord.lat, y.coord.lon);
     });
 }
 
+//fetches UV data
+function fetchesUV(lat, lon) {
+    
+    let currentUV =`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily,alerts&appid=${apiKey}`;
+
+    fetch(currentUV)
+    .then(response => response.json())
+    .then(y => {
+        let currentWeatherContainer = document.getElementById("weather-current");
+        console.log(y);
+        let uvi = y.current.uvi;
+        //colors uvi levels
+        let color = "bg-success";
+        if (uvi >2 && uvi <= 7) {
+            color = "bg-warning";
+        }
+        if (uvi > 7) {
+            color = "bg-danger";
+        }
+        currentWeatherContainer.insertAdjacentHTML('beforeEnd', `<br>UV Index:&nbsp;<span class="badge ${color}">${uvi}</span><br><br>`);
+    });
+}
 
 //getting 5 day weather forecast in a city specified  
 function fetchFiveDayWeather(city) {
@@ -43,6 +69,7 @@ function search() {
     fetchFiveDayWeather(searchValue);
 }
 
+// Calculating Fahrenheit temperature
 function kelvinToFahrenheit (kelvinDegrees) {
     // Celsius is 273 degrees less than Kelvin
 const celsius = kelvinDegrees - 273;
@@ -59,8 +86,6 @@ function getSearchedCities() {
     for (var i = 0; i < localStorage.length; i++){
         const localStorageKey = localStorage.key(i);
        
-
-
         if (!localStorageKey.includes("WeatherApp_")) {
             continue;
         }
@@ -74,9 +99,24 @@ function getSearchedCities() {
     }
 }
 
+//shows chosen city from grey buttons on the main card
 function storageCity(button) {
     console.log(button);
+
+    let city = button.innerHTML;
+    fetchCurrentWeather(city);
+    fetchFiveDayWeather(city);
 }
 
-
 getSearchedCities();
+
+// function name() {
+    
+//     let oldCities = `<button type="button" class="btn btn-secondary" onclick="storageCity(this)" >${city}</button>`;
+//     let historyContainer = document.getElementById("search-history");
+//     historyContainer.insertAdjacentHTML('beforeEnd', oldCities);
+// }
+
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+
